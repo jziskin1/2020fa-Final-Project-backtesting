15ed9d5186c703d3144aca9bb030a5c41c2c4074
jziskin1 <joz759@g.harvard.edu>
2020-11-29 17:43:08 -0500

    Created GetHistoricalData Task

--- backtester/scrape.py
+++ backtester/scrape.py
@@ -1,8 +1,39 @@
-""" Luigi tasks to webscrape stock data, download locally, and clean
+""" Luigi tasks and functions used to webscrape stock data to use in
+backtesting and final report
 
-Scrape: External Task that returns a target of stock data
+GetHistoricalData: Luigi Task that downloads
 
-Cleaned: Luigi Task that cleans the data and adds any necessary
-columns for backtesting
 
 """
+
+from luigi import Task, Parameter, build
+import dask.dataframe as dd
+import yfinance as yf
+
+from csci_utils.luigi.task import Requirement, Requires, TargetOutput
+from csci_utils.luigi.dask.target import ParquetTarget
+from utils import get_range
+
+
+class GetHistoricalData(Task):
+    # TODO: Add parameter based on strategy to only download needed columns.
+    # TODO: Add date function to update
+    # TODO: Salted
+
+    symbol = Parameter(default="AAPL")
+    interval = Parameter(default="1d")
+    output = TargetOutput(
+        file_pattern="data/{symbol}/{interval}/rawdata/", target_class=ParquetTarget
+    )
+
+    def run(self):
+        stock = yf.Ticker(self.symbol)
+        df = stock.history(
+            interval=self.interval, period=get_range(self.interval)
+        ).iloc[:, :5]
+        ddf = dd.from_pandas(df, npartitions=1)
+        self.output().write_dask(ddf, compression="gzip")
+
+
+if __name__ == "__main__":
+    build([GetHistoricalData(symbol="BA", interval="1d")], local_scheduler=True)
